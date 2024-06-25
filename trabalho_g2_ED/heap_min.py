a369099845a72e7a0c2dc36fbff7111fed821971
José Arcangelo de Souza Garlet <127755871+JoseArcangelo@users.noreply.github.com>
2024-06-25 10:33:06 -0300

    Update heap_min.py

--- trabalho_g2_ED/heap_min.py
+++ trabalho_g2_ED/heap_min.py
@@ -13,7 +13,7 @@ def ordena_prioridade(heap, index):
         else:
             break
 
-def inserir(heap_min, valor):
+def insert(heap_min, valor):
     heap_min.count += 1
     heap_min.heap_list.append(valor)
     ordena_prioridade(heap_min, heap_min.count - 1)
@@ -34,12 +34,15 @@ def heapify_down(heap, index):
         heap.heap_list[index], heap.heap_list[smallest] = heap.heap_list[smallest], heap.heap_list[index]
         heapify_down(heap, smallest)
 
-def remover(heap_min):
+def remove(heap_min):
     if heap_min.count == 0:
         return heap_min
     
+    
     heap_min.heap_list[0], heap_min.heap_list[-1] = heap_min.heap_list[-1], heap_min.heap_list[0]
+    print(heap_min.heap_list)
     min_value = heap_min.heap_list.pop()
+    
     heap_min.count -= 1
     
     if heap_min.count > 0:
@@ -48,20 +51,43 @@ def remover(heap_min):
     return heap_min
 
 def valor_minimo(heap_min):
-    return heap_min.heap_list[0] if heap_min.count > 0 else None
+    return heap_min.heap_list[0] 
 
 heap_min = heapBinary()
-heap_min = inserir(heap_min, 10)
-heap_min = inserir(heap_min, 9)
-heap_min = inserir(heap_min, 8)
-heap_min = inserir(heap_min, 7)
-heap_min = inserir(heap_min, 6)
-heap_min = inserir(heap_min, 5)
+heap_min = insert(heap_min, 10)
+heap_min = insert(heap_min, 9)
+heap_min = insert(heap_min, 8)
+heap_min = insert(heap_min, 7)
+heap_min = insert(heap_min, 6)
+heap_min = insert(heap_min, 5)
+heap_min = insert(heap_min, 0)
 
 print("Heap após inserções:", heap_min.heap_list)
+#            5
+#     7             6
+#  10   8         9
 
-heap_min = remover(heap_min)
+heap_min = remove(heap_min)
+print("Heap após remover o mínimo:", heap_min.heap_list)
+# 1)
+#            9
+#     7             6
+#  10   8         5
+
+# 2)
+#            9
+#     7             6
+#  10   8         
+
+# 3)
+#            6
+#     7             9
+#  10   8         
+
+heap_min = remove(heap_min)
 print("Heap após remover o mínimo:", heap_min.heap_list)
 
 menor_valor = valor_minimo(heap_min)
 print("Menor valor atual:", menor_valor)
+
+print("Heap após inserções:", heap_min.heap_list)
